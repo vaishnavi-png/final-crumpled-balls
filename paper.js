5649ccb5ace6db5cd843ceff3eec6387b5e7112a
vaishnavi-png <63649802+vaishnavi-png@users.noreply.github.com>
2021-01-22 11:57:31 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,35 @@
+class Paper{
+
+    constructor(x,y,radius){
+
+	var options={
+        restitution:0.8,
+        friction:1.0,
+        density:1.0
+        
+     
+            
+        
+            }
+            this.radius=radius;
+            this.image=loadImage("paper.png");
+            
+            this.body=Bodies.circle(x,y,radius,options);
+        World.add(world,this.body);
+        
+        }   
+
+
+        
+
+display() { 
+  var paperpos=this.body.position;
+  var angle=this.body.angle;
+   push() 
+   translate(paperpos.x, paperpos.y);
+   rotate(angle);
+    imageMode(RADIUS); 
+
+    image(this.image,0,0,this.radius, this.radius); 
+    pop() }
+}
